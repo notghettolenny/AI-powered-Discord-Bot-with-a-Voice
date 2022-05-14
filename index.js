a60155cae2f979afe071f3bcc1ebbe0dec86a57b
notghettolenny <79231993+notghettolenny@users.noreply.github.com>
2022-05-14 22:48:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,3 +28,52 @@ const textToSpeech = new TextToSpeechV1({
   }),
   serviceUrl: process.env.TTS_URL,
 });
+const textToSpeech = new TextToSpeechV1({
+  authenticator: new IamAuthenticator({
+    apikey: process.env.TTS_KEY,
+  }),
+  serviceUrl: process.env.TTS_URL,
+});
+
+async function sendAndPlayMessage(message) {
+  try {
+    // Start a new session with our asssistant
+    const sessionId = (await assistant.createSession({ assistantId: process.env.ASSISTANT_ID  })).result.session_id;
+    // Send the user's question to our assistant
+    assistant.message({
+      input: { text: message.content.substring(1) },
+      assistantId: process.env.ASSISTANT_ID,
+      sessionId: sessionId
+    })
+    .then(response => {
+      //  Convert the response into a readable message for our users
+      let text =  JSON.stringify(response.result.output.generic[0].text, null, 2); // pass the value to the global variable
+
+      // Join the same voice channel of the author of the message
+      if (message.member.voice.channel) {
+
+        // This configures the voice that will be used, the file type that is returned and the text that will be spoken
+        const synthesizeParams = {
+          text: response.result.output.generic[0].text,
+          accept: 'audio/wav',
+          voice: 'en-US_AllisonV3Voice',
+        };
+
+        // This is sending the request
+        textToSpeech.synthesize(synthesizeParams)
+        .then(response => {
+          // This is here specifically for .wav files following IBM's Text to Speech guides
+          return textToSpeech.repairWavHeaderStream(response.result);
+        })
+        .then(buffer => {
+          // Here we are creating a file and saving the returned value from the Text to Speech instance into it
+          fs.writeFile(`./audio.wav`, buffer, (err) => {
+            if (err) console.error("error" + err);
+            // Now we have the bot join the voice channel that the sender is in
+            const connection = message.member.voice.channel.join().then(connection => {
+              // And we get the bot to play the audio file
+              const dispatcher = connection.play('audio.wav')
+              .on('start', () => {
+                console.log('audio.wav is now playing!'); // Checking the file is played
+                message.reply(text); // While the audio is playing, we can send the message into the chat
+              })
