e5f83c471f3fe23ebb19763e21d72b3380a2caa4
notghettolenny <79231993+notghettolenny@users.noreply.github.com>
2022-05-14 22:46:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ const client = new Discord.Client();
 const AssistantV2 = require('ibm-watson/assistant/v2'); // Add Watson Assistant
 const TextToSpeechV1 = require('ibm-watson/text-to-speech/v1'); // Add Text to Speech
 const { IamAuthenticator } = require('ibm-watson/auth'); // Add Watson Authentication
+
+// Add our assistant from the IBM Watson Assistant service
+const assistant = new AssistantV2({
+  version: '2021-06-14',
+  authenticator: new IamAuthenticator({
+    apikey: process.env.ASSISTANT_KEY,
+  }),
+  serviceUrl: process.env.ASSISTANT_URL,
+});
+
+const textToSpeech = new TextToSpeechV1({
+  authenticator: new IamAuthenticator({
+    apikey: process.env.TTS_KEY,
+  }),
+  serviceUrl: process.env.TTS_URL,
+});
